70a3e925393e5fffdc307ace24c69be256fb6b2b
FernoNowsky <246327@edu.p.lodz.pl>
2025-10-31 17:36:51 +0100

    Implemented initial reviewsApi for future integration and development

---
+++ src/api/reviewsApi.ts
@@ -0,0 +1,23 @@
+import { apiRequest } from "@/lib/apiRequest";
+import type { PaginatedResponse } from "./types/common.types";
+
+export interface Review {
+  id: number
+  productId: number
+  userId: number
+  content: string
+  status: "PENDING" | "APPROVED" | "REJECTED"
+  rating: number
+  createdAt: Date
+  updatedAt: Date
+}
+
+export const ReviewsAPI = {
+
+    getByProductId: (productId: number) =>
+        apiRequest<PaginatedResponse<Review>>(`/reviews/${productId}`, {
+            params: {
+                productId: productId
+            },
+        }),
+};
